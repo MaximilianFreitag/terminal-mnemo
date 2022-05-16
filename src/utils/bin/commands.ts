915205da38f1264d0a4394833f7dd710e1d33a99
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-05-16 21:21:00 +0200

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -118,7 +118,7 @@ export const date = async (args: string[]): Promise<string> => {
 
 
 export const sudo = async (args?: string[]): Promise<string> => {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+  window.open('https://www.youtube.com/watch?v=176WobDw3YA', '_blank'); 
   return `Zugriff verweigert: Nur Max Mnemo hat Zugriff auf admin Rechte `;
 };
 
